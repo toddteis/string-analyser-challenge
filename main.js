451378b24e9b6f5cf977635581c45e8d839ef347
toddt <toddteis@gmail.com>
2022-07-16 14:06:03 +1000

    Added strAnalyser module and charOccurence() method.

---
+++ main.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const strAnalyser = (() => {
+  const charOccurence = (str) => {
+    const result = str;
+
+    return console.log(result);
+  };
+
+  return {
+    charOccurence,
+  };
+})();
