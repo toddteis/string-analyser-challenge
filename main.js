c69d9c63ba66187cec54538295a4a83982015aac
toddt <toddteis@gmail.com>
2022-07-17 08:15:50 +1000

    Added Factory Module and created charInfo() method. Added basic logic in strAnalyser.charOccurence() to test factory.charInfo().

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+const factory = (() => {
+  const charInfo = (char, occurence) => ({ char, occurence });
+
+  return {
+    charInfo,
+  };
+})();
+
 // eslint-disable-next-line no-unused-vars
 const strAnalyser = (() => {
   const charOccurence = (str) => {
-    const result = str;
-
-    return console.log(result);
+    const charArray = str.toLowerCase().split('');
+    const tally = [];
+    for (let i = 0; i < charArray.length; i += 1) {
+      const element = charArray[i];
+      tally.push(factory.charInfo(element, 1));
+    }
+    return console.log(tally);
   };
 
   return {
